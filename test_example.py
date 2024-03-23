a141d8617e119eaff17859abef5b9ec524a0fa84
misha <mishafrancis@gmail.com>
2024-03-23 23:16:51 +0100

    update

--- test_example.py
+++ test_example.py
@@ -8,6 +8,8 @@ def test_has_title(page: Page):
     # Expect a title "to contain" a substring.
     expect(page).to_have_title(re.compile("Playwright"))
 
+    print('This is a check-point')
+
 def test_get_started_link(page: Page):
     page.goto("https://playwright.dev/")
 
