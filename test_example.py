ab3fea09abca65f8141150e3c7a816718e1e3202
misha <mishafrancis@gmail.com>
2024-03-29 00:01:01 +0100

    Jenkins check

--- test_example.py
+++ test_example.py
@@ -8,7 +8,7 @@ def test_has_title(page: Page):
     # Expect a title "to contain" a substring.
     expect(page).to_have_title(re.compile("Playwright"))
 
-    print('This is a check-point for me')
+    print('This is a check-point for me. Thanks !')
 
 def test_get_started_link(page: Page):
     page.goto("https://playwright.dev/")
