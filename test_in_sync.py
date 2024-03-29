b6173a70498464a26372ec41b41952dd1ece473f
Misha Francis <mishafrancis@gmail.com>
2024-03-29 13:52:27 +0100

    sync_tests

--- test_in_sync.py
+++ test_in_sync.py
@@ -1,11 +1,12 @@
-import re 
+import re
+from turtle import delay 
 from playwright.sync_api import Page, expect
 
 def test_has_title(page: Page):
     
     page.goto("https://playwright.dev/")
 
-    # Expect a title "to contain" a substring.
+    # Expect a title
     expect(page).to_have_title(re.compile("Playwright"))
 
     print('This is a check-point for me. Thanks !')
@@ -22,3 +23,16 @@ def test_get_started_link(page: Page):
 
 def test_get_started_print():
     print("Get started")
+
+def test_fedex_home_page_links(page: Page):
+
+    page.goto("https://www.fedex.com/en-nl/home.html")
+    
+    # Expect a title
+    expect(page).to_have_title(re.compile("FedEx | Express Delivery, Courier & Shipping Services | Netherlands"))
+    
+    # Expect below url
+    expect(page).to_have_url('https://www.fedex.com/en-nl/home.html')
+    print(page.url)
+
+    page.close()
