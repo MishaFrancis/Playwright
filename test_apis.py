1c727d63db01003970d16ae590601e1ef544f338
misha <mishafrancis@gmail.com>
2024-04-06 19:23:15 +0200

    api

--- test_apis.py
+++ test_apis.py
@@ -1,5 +1,22 @@
 import requests
 import json
+import pytest
+from typing import Generator
+from playwright.sync_api import Playwright, APIRequestContext
+
+@pytest.fixture(scope="session")
+def api_request_context(
+    playwright: Playwright,
+    ) -> Generator[APIRequestContext, None, None]:
+    headers = {
+        # We set this header per GitHub guidelines.
+        # Add authorization token to all requests.
+        # Assuming personal access token available in the environment.
+    }
+    request_context = playwright.request.new_context(
+    )
+    yield request_context
+    request_context.dispose()
 
 
 def test_apis_get():
@@ -27,3 +44,22 @@ def test_apis_post():
 
     x = requests.post(url, json = myobj)
     print(x.status_code)
+
+
+def test_api_get_post(api_request_context: APIRequestContext) -> None:
+    url = 'https://www.w3schools.com/python/demopage.php'
+    print(url)
+    data = {
+            "year": 2019,
+            "price": 1849.99,
+            "CPU model": "Intel Core i9",
+            "Hard disk size": "1 TB"
+           }
+    new_issue = api_request_context.post(url, data=data)
+    assert new_issue.ok
+    print(new_issue.status)
+
+    issues = api_request_context.get(url)
+    assert issues.ok
+
+    print(issues.status_text)
