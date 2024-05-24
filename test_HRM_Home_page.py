91fbdf99e14ba0b3bb7a76bee01a9f72eb4c3208
Misha Francis <mishafrancis@gmail.com>
2024-05-24 11:42:08 +0200

    class update

--- test_HRM_Home_page.py
+++ test_HRM_Home_page.py
@@ -28,7 +28,8 @@ def test_Orange_HRM_Home_Page(playwright: Playwright) -> None:
 
     ## Logout
     # page.get_by_role("banner").get_by_role("img", name="profile picture").click()
-    page.locator("xpath=/html/body/div/div[1]/div[1]/header/div[1]/div[2]/ul/li/span/i").click()  ## XPath way
+    # page.locator("xpath=/html/body/div/div[1]/div[1]/header/div[1]/div[2]/ul/li/span/i").click()  ## XPath way
+    page.wait_for_selector('[class="oxd-icon bi-caret-down-fill oxd-userdropdown-icon"]').click()   ## Using class of the dropdown arrow
     page.get_by_role("menuitem", name="Logout").click()
     expect(page.get_by_role("heading", name="Login")).to_be_visible()
     page.get_by_role("heading", name="Login").highlight()
